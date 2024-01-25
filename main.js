2e5cf7c8aba52615153478f0519b104cfdcac853
Micah Coldiron <mcc4339@rit.edu>
2024-01-25 14:20:10 -0800

    Food item is done

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+const item = document.getElementById("foodTypeInput");
+const cal = document.getElementById("calorieAmountInput");
+const done = document.getElementById("submitButton");
+class food {
+    calories;
+    names;
+    food (c, n) {
+        calories = parseInt(c);
+        names = n;
+    }
+    print () {
+        return "This food is a " + names + ", and it contains " + calories + " calories.";
+    }
+}
 document.addEventListener("DOMContentLoaded", function (event) {
-
+    
     
 });
-
