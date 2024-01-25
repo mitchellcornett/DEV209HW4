ddd23e989728c2aed07253e10aae27750a1caec0
mitchellcornett <mitchell.cornett2@gmail.com>
2024-01-25 14:03:59 -0800

    set up basic html

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById("button1").addEventListener("click", function () {
-
-        document.getElementById("textbox1").value = "success";
-    });
+    
 });
 
