61a1ca45f643c8bd7d93492878e4b15e34d9562c
Micah Coldiron <mcc4339@rit.edu>
2024-01-25 14:28:08 -0800

    Wrote the rest of the js code.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const item = document.getElementById("foodTypeInput");
 const cal = document.getElementById("calorieAmountInput");
 const done = document.getElementById("submitButton");
+const out = document.getElementById("totalCaloriesDisplay");
 class food {
     calories;
     names;
@@ -11,8 +12,18 @@ class food {
     print () {
         return "This food is a " + names + ", and it contains " + calories + " calories.";
     }
+    getCals () {
+        return this.calories;
+    }
 }
 document.addEventListener("DOMContentLoaded", function (event) {
-    
+    let foods = [];
+    let cals = 0;
+    done.addEventListener("click", function () {
+        f = new new food(item.value, cal.value);
+        foods.push(f);
+        cals += f.getCals();
+        out.innerHTML = cals;
+    });
     
 });
